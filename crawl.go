5f4c5c8f85c66f8e613dc9767ac394c2b68951d4
Jack Danger Canty <jackdanger@squareup.com>
2014-05-10 12:43:59 -0700

    incorporating parts of the blog post

--- crawl.go
+++ crawl.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+  "crypto/tls"
   "flag"
   "fmt"
+  "github.com/jackdanger/collectlinks"
+  "net/http"
+  "net/url"
   "os"
 )
 
@@ -12,15 +16,65 @@ func usage() {
   os.Exit(2)
 }
 
+var visited = make(map[string]bool)
+
 func main() {
   flag.Usage = usage
   flag.Parse()
 
   args := flag.Args()
+  fmt.Println(args)
   if len(args) < 1 {
     usage()
-    fmt.Println("A starting web page is necessary to crawl the internet")
+    fmt.Println("Please specify start page")
     os.Exit(1)
   }
-  Start(args[0])
+
+  queue := make(chan string)
+
+  go func() { queue <- args[0] }()
+
+  for uri := range queue {
+    enqueue(uri, queue)
+  }
+}
+
+func enqueue(uri string, queue chan string) {
+  fmt.Println("fetching", uri)
+  visited[uri] = true
+  transport := &http.Transport{
+    TLSClientConfig: &tls.Config{
+      InsecureSkipVerify: true,
+    },
+  }
+  client := http.Client{Transport: transport}
+  resp, err := client.Get(uri)
+  if err != nil {
+    return
+  }
+  defer resp.Body.Close()
+
+  links := collectlinks.All(resp.Body)
+
+  for _, link := range links {
+    absolute := fixUrl(link, uri)
+    if uri != "" {
+      if !visited[absolute] {
+        go func() { queue <- absolute }()
+      }
+    }
+  }
+}
+
+func fixUrl(href, base string) string {
+  uri, err := url.Parse(href)
+  if err != nil {
+    return ""
+  }
+  baseUrl, err := url.Parse(base)
+  if err != nil {
+    return ""
+  }
+  uri = baseUrl.ResolveReference(uri)
+  return uri.String()
 }
