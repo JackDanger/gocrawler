a47e2dbc5ebadc38ff5fbd2cf3fc5751c19b0fd2
Gabriel Gilder <gabriel@gabrielgilder.com>
2014-05-11 17:38:35 -0700

    Run gofmt
    Tabs! :crying_cat_face:

--- crawl.go
+++ crawl.go
@@ -1,80 +1,80 @@
 package main
 
 import (
-  "crypto/tls"
-  "flag"
-  "fmt"
-  "github.com/jackdanger/collectlinks"
-  "net/http"
-  "net/url"
-  "os"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"github.com/jackdanger/collectlinks"
+	"net/http"
+	"net/url"
+	"os"
 )
 
 func usage() {
-  fmt.Fprintf(os.Stderr, "usage: crawl http://example.com/path/file.html\n")
-  flag.PrintDefaults()
-  os.Exit(2)
+	fmt.Fprintf(os.Stderr, "usage: crawl http://example.com/path/file.html\n")
+	flag.PrintDefaults()
+	os.Exit(2)
 }
 
 var visited = make(map[string]bool)
 
 func main() {
-  flag.Usage = usage
-  flag.Parse()
+	flag.Usage = usage
+	flag.Parse()
 
-  args := flag.Args()
-  fmt.Println(args)
-  if len(args) < 1 {
-    usage()
-    fmt.Println("Please specify start page")
-    os.Exit(1)
-  }
+	args := flag.Args()
+	fmt.Println(args)
+	if len(args) < 1 {
+		usage()
+		fmt.Println("Please specify start page")
+		os.Exit(1)
+	}
 
-  queue := make(chan string)
+	queue := make(chan string)
 
-  go func() { queue <- args[0] }()
+	go func() { queue <- args[0] }()
 
-  for uri := range queue {
-    enqueue(uri, queue)
-  }
+	for uri := range queue {
+		enqueue(uri, queue)
+	}
 }
 
 func enqueue(uri string, queue chan string) {
-  fmt.Println("fetching", uri)
-  visited[uri] = true
-  transport := &http.Transport{
-    TLSClientConfig: &tls.Config{
-      InsecureSkipVerify: true,
-    },
-  }
-  client := http.Client{Transport: transport}
-  resp, err := client.Get(uri)
-  if err != nil {
-    return
-  }
-  defer resp.Body.Close()
+	fmt.Println("fetching", uri)
+	visited[uri] = true
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+	client := http.Client{Transport: transport}
+	resp, err := client.Get(uri)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
 
-  links := collectlinks.All(resp.Body)
+	links := collectlinks.All(resp.Body)
 
-  for _, link := range links {
-    absolute := fixUrl(link, uri)
-    if uri != "" {
-      if !visited[absolute] {
-        go func() { queue <- absolute }()
-      }
-    }
-  }
+	for _, link := range links {
+		absolute := fixUrl(link, uri)
+		if uri != "" {
+			if !visited[absolute] {
+				go func() { queue <- absolute }()
+			}
+		}
+	}
 }
 
 func fixUrl(href, base string) string {
-  uri, err := url.Parse(href)
-  if err != nil {
-    return ""
-  }
-  baseUrl, err := url.Parse(base)
-  if err != nil {
-    return ""
-  }
-  uri = baseUrl.ResolveReference(uri)
-  return uri.String()
+	uri, err := url.Parse(href)
+	if err != nil {
+		return ""
+	}
+	baseUrl, err := url.Parse(base)
+	if err != nil {
+		return ""
+	}
+	uri = baseUrl.ResolveReference(uri)
+	return uri.String()
 }
